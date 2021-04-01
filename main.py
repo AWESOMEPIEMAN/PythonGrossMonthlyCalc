05ac884d232c01a8ae7fc54ab2bcb2ecf6306a0b
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-04-02 04:29:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ payment = int(input("Please input your hourly wage : "))
 week_n = int(input("Please input the amount of hours you work in a week : "))
 
 annual_pay = payment*week_n*52
-
 gross_mon = annual_pay/12
 
 print("Your gross monthly income is : ", gross_mon)
